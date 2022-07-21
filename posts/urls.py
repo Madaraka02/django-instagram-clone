77f740f84530c9cdabbabedbc7e6a4e9b8f0af95
Madaraka02 <victormadaraka@gmail.com>
2022-07-21 21:51:15 +0300

    added like and dislike feature

--- posts/urls.py
+++ posts/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('profile/<int:id>/', userProfile, name="profile"),
     path('profile/<int:id>/follow/', follow, name="follow"),
     path('profile/<int:id>/unfollow/', unfollow, name="unfollow"),
+    path('post/<int:id>/dislike/', dislike, name="dislike"),
+    path('post/<int:id>/like/', like, name="like")
+
 
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponseRedirect
+
 from users.models import *
 from .forms import *
 from .models import *
@@ -29,67 +31,68 @@ def home(request):
 
 def postdetail(request, id):
     # get post
+    
     # post model add likes and dislikes manytomanyrelated_name=likes/dislike
     # 
     pass    
 
-def like(request,pk):
+def like(request,id):
     # get post by id
+    post = get_object_or_404(Post, id=id)
 
-    # is_disliked =False
-    # for dislike in post.dislikes.all():
+    is_disliked =False
+    for dislike in post.dislikes.all():
 
-    #   if is_disliked == request.user: -> user has already disliked post
-    #       is_disliked =True
-    #       break
+        if dislike == request.user: # user has already disliked post
+            is_disliked =True
+            break
 
 
-    # is_liked =False
-    # for like in post.likes.all():
-    #   if like == request.user: 
-    #       is_liked =True
-    #       break
-    # if not is_liked:
-    #       post.likes.add(request.user)
-    # if is_liked:
-    #       post.likes.remove(request.user)
+    is_liked =False
+    for like in post.likes.all():
+        if like == request.user: 
+            is_liked =True
+            break
+    if not is_liked:
+        post.likes.add(request.user)
+    if is_liked:
+        post.likes.remove(request.user)
 
-    pass
+    next = request.POST.get('next','/')
 
-def dislike(request,pk):
+    return redirect('home')
+    
+
+def dislike(request,id):
     # get post by id
+    post = get_object_or_404(Post, id=id)
     # check if user has already liked the post if true remove the like
-    # is_liked =False
-    # for like in post.likes.all():
-    #   if like == request.user: 
-    #       is_liked =True
-    #       break
+    is_liked =False
+    for like in post.likes.all():
+        if like == request.user: 
+            is_liked =True
+            break
 
     # if is_disliked:
-    #       post.dislikes.remove(request.user)
+    #     post.dislikes.remove(request.user)
 
-    # if is_liked:
-    #       post.likes.remove(request.user)
+    if is_liked:
+        post.likes.remove(request.user)
 
-    # is_disliked =False
-    # for dislike in post.dislikes.all():
+    is_disliked =False
+    for dislike in post.dislikes.all():
 
-    #   if is_disliked == request.user: -> user has already disliked post
-    #       is_disliked =True
-    #       break
-    # if not is_disliked:
-    #       post.dislikes.add(request.user)
-    # if is_disliked:
-    #       post.dislikes.remove(request.user)
-    # next = request.POST.get('next','/)
-    # return HttpResponseRedirect(next)
+        if dislike == request.user: # user has already disliked post
+            is_disliked =True
+            break
+    if not is_disliked:
+        post.dislikes.add(request.user)
+    if is_disliked:
+        post.dislikes.remove(request.user)
+    next = request.POST.get('next','/')
+
+    return redirect('home')
 
 
-    pass
 # create urls for like and dislike : post/<int:pk>/like
-# template
-# <form method=post action={url 'like' post.pk}> 
-# csrf
-# <input type=hidden name=next value={{request.path}}/>
-# <btn submit>Like {{post.likes.all.count}}</btn>
-# </form>
+    # <!-- action={% url 'dislike' post.pk %} -->
